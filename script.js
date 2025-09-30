f99665aff402d17c9990fbd9e00244b3b9118722
iGeek-OS2 <88642228+iGeek-OS2@users.noreply.github.com>
2025-09-30 12:19:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs
 
 document.addEventListener('DOMContentLoaded', () => {
    
-    const PROXY_SERVER_URL = 'https://flashmaker-api-proxy.dfg147147147.workers.dev'; // 必ずご自身のURLに書き換えてください
+    const PROXY_SERVER_URL = 'https://flashmaker-api-proxy.dfg147147147.workers.dev'; 
 
     // --- 状態管理 ---
     const state = {
